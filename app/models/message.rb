9e31ede0b4ca8a7f030e0563fda8dbd822554b83
Vladyslav Knysh <vladyslav.knysh@teladochealth.com>
2023-01-09 22:36:28 +0200

    Add direct messages functionality

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,10 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
   after_create_commit { broadcast_append_to room }
+  before_create :confirm_participant
+
+  def confirm_participant
+    return unless room.is_private
+    errors.add(:base, 'You are not a participant of this room') unless room.participants.exists?(user_id: user_id)
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,19 @@
 class Room < ApplicationRecord
   validates_uniqueness_of :name
   scope :public_rooms, -> { where(is_private: false) }
-  after_create_commit { broadcast_append_to "rooms" }
+  after_create_commit { broadcast_if_public }
   has_many :messages
+  has_many :participants, dependent: :destroy
+
+  def broadcast_if_public
+    broadcast_append_to 'rooms' unless self.is_private
+  end
+
+  def self.create_private_room(users, name)
+    single_room = Room.create(name: name, is_private: true)
+    users.each do |user|
+      single_room.participants.create(user_id: user.id)
+    end
+    single_room
+  end
 end
