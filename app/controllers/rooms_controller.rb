d8c41976d349ff5108f16059c101b3c669894b8c
Vladyslav Knysh <vladyslav.knysh@teladochealth.com>
2023-01-09 21:44:34 +0200

    Add messaging functionality

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,12 +4,21 @@ class RoomsController < ApplicationController
   def index
     @room = Room.new
     @rooms = Room.public_rooms
-
-    @single_room = Room.find(params[:id])
-
     @users = User.all_except(current_user)
+    render :index
   end
 
+  def show
+    @single_room = Room.find(params[:id])
+    @room = Room.new
+    @rooms = Room.public_rooms
+
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :asc)
+
+    @users = User.all_except(current_user)
+    render :index
+  end
   def create
     @room = Room.create(name: params["room"]["name"])
   end
