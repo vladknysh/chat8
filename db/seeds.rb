f2f808f8523b0b85a5b959acd2c8091ab096e19f
Vladyslav Knysh <vladyslav.knysh@teladochealth.com>
2023-01-10 09:28:48 +0200

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+vlad = User.create(email: 'vlad@vlad.com',
+                   password: 'password',
+                   password_confirmation: 'password',
+                   role: 'admin')
+User.create(email: 'asdf@asdf.com',
+            password: 'password',
+            password_confirmation: 'password')
+User.create(email: 'alex@alex.com',
+            password: 'password',
+            password_confirmation: 'password')
+vlad.joined_rooms << Room.create(name: 'General', is_private: false)
+vlad.joined_rooms << Room.create(name: 'Testing', is_private: false)
