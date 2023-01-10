05b0641ec3405c23f01c82cc2cdd9c14bc6b7294
Vladyslav Knysh <vladyslav.knysh@teladochealth.com>
2023-01-10 06:26:16 +0200

    Add message attachments with previews

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,9 +3,23 @@ class Message < ApplicationRecord
   belongs_to :room
   after_create_commit { broadcast_append_to room }
   before_create :confirm_participant
+  has_many_attached :attachments, dependent: :destroy
+
+  def chat_attachment(index)
+    target = attachments[index]
+    return unless attachments.attached?
+
+    if target.image?
+      target.variant(resize_to_limit: [150, 150]).processed
+    elsif target.video?
+      target.variant(resize_to_limit: [150, 150]).processed
+    end
+  end
 
   def confirm_participant
     return unless room.is_private
-    errors.add(:base, 'You are not a participant of this room') unless room.participants.exists?(user_id: user_id)
+
+    is_participant = Participant.where(user_id: user.id, room_id: room.id).first
+    throw :abort unless is_participant
   end
 end
