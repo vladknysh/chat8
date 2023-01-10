d2ded13559154ea04cf2305a8a591ad027949cf7
Vladyslav Knysh <vladyslav.knysh@teladochealth.com>
2023-01-10 06:15:34 +0200

    Add user statuses

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,15 +1,17 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :set_status
   def index
     @room = Room.new
     @rooms = Room.public_rooms
+
     @users = User.all_except(current_user)
-    render :index
+    render 'index'
   end
 
   def show
     @single_room = Room.find(params[:id])
+
     @room = Room.new
     @rooms = Room.public_rooms
 
@@ -17,9 +19,16 @@ class RoomsController < ApplicationController
     @messages = @single_room.messages.order(created_at: :asc)
 
     @users = User.all_except(current_user)
-    render :index
+    render 'index'
   end
+
   def create
-    @room = Room.create(name: params["room"]["name"])
+    @room = Room.create(name: params['room']['name'])
+  end
+
+  private
+
+  def set_status
+    current_user.update!(status: User.statuses[:online]) if current_user
   end
 end
